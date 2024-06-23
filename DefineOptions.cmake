3edf7595a19e831f3d84b870160a509441be73d1
Craig Kolb <craig@kolb.net>
2024-06-23 09:28:40 -0700

    DLL

--- DefineOptions.cmake
+++ DefineOptions.cmake
@@ -1,4 +1,4 @@
 option(OPTION_OMP "Build with OpenMP support" OFF)
 option(VERBOSE "Build in verbose mode" OFF)
 option(PICKY_DEVELOPER "Build with picky developer flags" OFF)
-option(BUILD_SHARED_LIBS "Build shared libraries" OFF)
+option(BUILD_SHARED_LIBS "Build shared libraries" ON)
